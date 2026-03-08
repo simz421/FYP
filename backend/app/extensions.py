8fccf701f1d009a4a65a84383230e543fa1c2e2a
simz421 <simz.t168@gmail.com>
2026-03-08 21:00:13 +0200

    Delete backend directory

--- backend/app/extensions.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
-from apscheduler.schedulers.background import BackgroundScheduler
-
-scheduler = BackgroundScheduler()
-
-db = SQLAlchemy()
-cors = CORS()
